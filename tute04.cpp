e4cd16196f4b4c35f48385bcc9ba9c84908476e4
YBagya2000 <87359515+it21045394@users.noreply.github.com>
2021-07-19 16:52:59 +0000

    Done

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,15 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+
+long Factorial(int no){
+  int r,fac=1;
+  for(r=no ; r>=1 ; r--){
+    fac = fac*r;
+  }
+  return fac;
+}
+
+long nCr(int n, int r){
+  return Factorial(n)/(Factorial(r)*Factorial(n-r));
+}
